fe6c59231df71b88c412b82258b7e68dccb167c4
Andrewharber <harber.andrew@ymail.com>
2022-09-21 11:50:16 -0400

    commit tree 2d3d20f04fb2c634a130aa0ce2726a21d3ff0284

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name,email) {
+  const contacts = {
+      name: "",
+      email: ""
+    };
+
+  contacts.name = name
+  contacts.email = email
+  
+  const configurationObject = {
+     method: "POST",
+     headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+     },
+     body: JSON.stringify(contacts),
+    };
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (users) {
+        // const id = document.createElement('div')
+        // id.innerHTML = `${users.id}`
+        document.body.innerHTML = users.id
+    })
+    .catch(function (error) {
+      console.log(error.message)
+    //   const info = document.createElement('div');
+    //   info.innerHTML = `${replyWithError.message}`
+      
+      document.body.innerHTML = error.message
+    });
+};
